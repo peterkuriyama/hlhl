a76786ad0b9bad8dc07df9ed39a0760840dc3cdf
peterkuriyama <ptrkrym@uw.edu>
2016-10-03 14:11:35 -0700

    add angler samples as output

--- R/conduct_survey.R
+++ R/conduct_survey.R
@@ -17,21 +17,30 @@ conduct_survey <- function(ctl){
 
   nyear <- ctl$nyear
   survey_samples <- vector('list', length = nyear)
+  angler_samples <- vector('list', length = nyear)
+  fished_areas <- vector('list', length = nyear)
+  
   names(survey_samples) <- 1:nyear
+  names(angler_samples) <- 1:nyear
+  names(fished_areas) <- 1:nyear
 
   #initialize population
   init_area <- initialize_population(ctl = ctl)
 
   #Fish Area Once
-  after_first <-fish_population(init_area, ctl = ctl)
+  after_first <- fish_population(init_area, ctl = ctl)
   survey_samples[[1]] <- after_first$samples
+  angler_samples[[1]] <- after_first$angler_samples
+  fished_areas[[1]] <- after_first$updated_area
 
   temp_area <- after_first$updated_area #Define temporary area for use in the for loop
 
   #Loop over years of survey, specified in ctl
   for(kk in 2:nyear){
     temp <- fish_population(fish_area = temp_area, ctl = ctl)
-    survey_samples[[kk]] <- after_first$samples
+    survey_samples[[kk]] <- temp$samples
+    angler_samples[[kk]] <- temp$angler_samples
+    fished_areas[[kk]] <- temp$updated_area
 
     #Redefine the fishing area
     temp_area <- temp$updated_area
@@ -42,6 +51,9 @@ conduct_survey <- function(ctl){
   samples <- ldply(survey_samples)
   names(samples)[1] <- 'year'
 
+  angler_samples <- ldply(angler_samples)
+  names(angler_samples)[1] <- 'year'
+
   #calculate CPUE based on number of hooks
   nhooks <- ctl$nhooks
   cpue <- samples[, grep('drop',  names(samples))] / nhooks
@@ -51,8 +63,11 @@ conduct_survey <- function(ctl){
   
   cpue <- cbind(to_bind, cpue)
 
+  fished_areas[[nyear + 1]] <- init_area
+  names(fished_areas[[nyear + 1]]) <- 'init_area'
+
   #define output list
-  out <- list(init_area = init_area, end_area = temp_area, 
+  out <- list(fished_areas = fished_areas, angler_samples = angler_samples,
     samples = samples, cpue = cpue)
 
   return(out)
