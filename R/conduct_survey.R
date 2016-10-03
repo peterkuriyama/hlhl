be42dd7ce241bd745e99929ddaa3ea0c32b05a01
peterkuriyama <ptrkrym@uw.edu>
2016-10-03 10:13:56 -0700

    Clean up code

--- R/conduct_survey.R
+++ R/conduct_survey.R
@@ -3,26 +3,13 @@
 #' Give function arguments for
 #' This function initializes the spatial distribution of the fish population
 
-# #'@param fish_area Matrix with the distribution of fish
-# # '@param location_list list specifying rows and columns to survey in
-# #'@param location Data frame of locations with column for vessel, rows, and columns of fish are to fish in. 
-# #'@param scope the scope of fishing movement, default to 1 so fish in surrounding 1 cells can move in
-# #'@param nhooks number of hooks at the smallest sampling size
-# #'@param ndrops number of drops, default is 5 following hook and line protocol
-# #'@param process specify process by which fish are sampled, options are 'multinomial' and 'hypergeometric'
 #' @keywords survey
+#' @param ctl control list defined in make_ctl function
 #' @export
 #' @examples
-#' put example here dude
 #'
-
-#' init <- initialize_population(numrow = 10, numcol = 10, nfish = 10000, distribute = 'uniform',
-#'                                percent = .3, seed = 301)
-
-#' xx <- conduct_survey(fish_area = init, location_list = list(c(4, 10),
-#'                                                        c(8, 2),
-#'                                                        c(3, 3)), 
-#'                      scope = 1, nhooks = 15, ndrops = 5, process = 'equal_prob')
+#' ctl <- make_ctl()
+#' conduct_survey(ctl)
 
 conduct_survey <- function(ctl){
   # location, scope, nhooks, ndrops, 
@@ -51,10 +38,10 @@ conduct_survey <- function(ctl){
 
   }
 
+  #manipulate samples
   samples <- ldply(survey_samples)
   names(samples)[1] <- 'year'
 
-  
   #calculate CPUE based on number of hooks
   nhooks <- ctl$nhooks
   cpue <- samples[, grep('drop',  names(samples))] / nhooks
@@ -64,20 +51,7 @@ conduct_survey <- function(ctl){
   
   cpue <- cbind(to_bind, cpue)
 
-  #keep initial population matrix
-  # init_area <- fish_area
-
-  ####Make sure to set seed#####
-
-  # #Convert all the list structure into data frame structure
-  # temp <- fish_population(fish_area = fish_area, ctl = ctl)
-
-  # nhooks <- ctl$nhooks
-  
-  # #calculate cpue based on number of hooks
-  # cpue <- temp$samples[, grep('drop', names(temp$samples))] / nhooks
-  # cpue <- cbind(temp$samples[, 1:3], cpue)
-
+  #define output list
   out <- list(init_area = init_area, end_area = temp_area, 
     samples = samples, cpue = cpue)
 
