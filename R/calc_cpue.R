855d6988966f2ec8d29c93147a39e4478bdc2714
peterkuriyama <ptrkrym@uw.edu>
2016-10-05 08:58:11 -0700

    add in cpue to 75 hooks

--- R/calc_cpue.R
+++ R/calc_cpue.R
@@ -7,7 +7,7 @@
 #'@param cpue_method Method of calculating CPUE. Options are 'average' for calculating
 #' straight average by year.
 #'@export
-#'@example
+#'@examples
 #' ctl <- make_ctl(nhooks = 15, seed = 200, nfish = 10000, nyear = 50)
 #' out <- conduct_survey(ctl)
 #' cpue <- calc_cpue(out, ctl = ctl)
@@ -24,6 +24,27 @@ calc_cpue <- function(survey_res, ctl){
       as.data.frame %>% arrange(year) -> cpue
 
     cpue$avg_cpue <- cpue$avg_catch / ctl$nhooks
+  }
+
+  if(cpue_method == '75hooks'){
+# browser()
+    cpue <- melt(survey_res$samples, id.vars = c('year', 'vessel', 'x', 'y'))
+    cpue$year <- as.numeric(cpue$year)
+    
+    cpue %>% group_by(year, vessel, x, y) %>% summarize(tot_fish = sum(value),
+      avg_cpue = tot_fish / (ctl$nhooks * ctl$ndrops)) %>% group_by(year) %>% 
+      summarize(avg_cpue = mean(avg_cpue)) %>% as.data.frame -> cpue
+
+  }
+
+
+  if(cpue_method == 'weighted_average'){
+
+    #Weight by location
+    samps %>% group_by(x, y) %>% mutate(site_avg = mean(value)) %>% 
+      group_by(year, x, y) %>% mutate(year_site_avg = mean(value))
+    
+    samps %>% group_by(year, x, y) %>% mutate(avg = mean(value))
 
   }
 
