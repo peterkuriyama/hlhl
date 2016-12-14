a108402017d1a8c37dc092d50cc0b1215d8ebf98
peterkuriyama <ptrkrym@uw.edu>
2016-12-14 11:35:32 -0800

    add default values to function descriptions

--- R/make_ctl.R
+++ R/make_ctl.R
@@ -7,18 +7,18 @@
 #' @param nfish Number of fish to allocate among matrix
 #' @param seed Set seed if distribute == random, defaults to 300
 #' @param distribute Specify fish distribution to be 'uniform', 'patchy', or 'area' specific
-#' @param maxfish Maximum number of fish that can be sampled at a time
-#' @param percent percentage of area to sample. Only necessary if distribute == 'patchy'
+#' @param maxfish Maximum number of fish that can be sampled at a time; default is 10
+#' @param percent percentage of area to sample. Only necessary if distribute == 'patchy'; default is 0.3
 #' @param area Specify area to distribute fish, options are 'upperleft', 'upperright', 'lowerleft', 
 #' 'lowerright', 'upperhalf', 'lowerhalf', 'righthalf', 'lefthalf'  Only necessary if distribute == 'area'
 #'@param location Data frame of locations with column for vessel, rows, and columns of fish are to fish in. 
-#'@param scope the scope of fishing movement, default to 1 so fish in surrounding 1 cells can move in
-#'@param nhooks number of hooks at the smallest sampling size
-#'@param ndrops number of drops, default is 5 following hook and line protocol
+#'@param scope the scope of fishing movement, default to 1 so fish in surrounding 1 cells can move in; default is 1
+#'@param nhooks number of hooks at the smallest sampling size; default is 5
+#'@param ndrops number of drops, default is 5 following hook and line protocol; default is 5
 #'@param process specify process by which fish are sampled, options are 'multinomial', 'hypergeometric', and 'equal_prob'. 
 #' 'equal_prob' is based on CIE comments from 2012. 
-#'@param p0 Probability that fish detects gear, used in the hook_prob function
-#' @param nyear Number of years in survey
+#'@param p0 Probability that fish detects gear, used in the hook_prob function; default is 0.4  
+#' @param nyear Number of years in survey; default is 10
 #' @param browser Switch to turn on browser for debugging
 #' @param cpue_method Method of aggregating cpue. For use in calc_cpue function. Currently
 #' options are 'average', '75hooks' to aggregate by hooks, and 'weighted_average' is in development
