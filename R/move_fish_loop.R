85da76cf1a957aac50799d35ef14e0bfa95a4979
peterkuriyama <ptrkrym@uw.edu>
2017-01-26 20:20:06 -0800

    remove duplicated rows and fish counts

--- R/move_fish_loop.R
+++ R/move_fish_loop.R
@@ -8,6 +8,7 @@
 #' @export
 
 move_fish_loop <- function(location, ff){
+
   #Define the scope based on ctl file
   scope <- ctl$scope
   
@@ -16,6 +17,10 @@ move_fish_loop <- function(location, ff){
 
   #Store the number of fish that moved
   nfish_moved <- vector('list', length = 3)
+
+  #melt ff to keep track of fish moving in and out
+  ff_melt <- melt(ff)
+  names(ff_melt)[1:2] <- c('x', 'y')
 
   for(zz in 1:nrow(location)){
     moves <- define_movement(fish_area = ff, x = location[zz, 'x'],
@@ -28,28 +33,33 @@ move_fish_loop <- function(location, ff){
     moved$x <- orig_inds$Var1
     moved$y <- orig_inds$Var2
 
-    #Update the original fish area data frames
-    ff <- melt(ff)
-    update_these <- which(ff$Var1 %in% moved$x & ff$Var2 %in% moved$y)
+    update_these <- which(ff_melt$x %in% moved$x & ff_melt$y %in% moved$y)
+
 
     #Subtract moving fish
-    ff[update_these, 'value'] <- ff[update_these, 'value'] - moved$moving
-    ff[update_these, 'value'] <- moved$moved
-
-    #convert ff to a matrix
-    ff <- matrix(ff$value, nrow = ctl$numrow, ncol = ctl$numcol)
+    ff_melt[update_these, 'value'] <- ff_melt[update_these, 'value'] - moved$moving
+    
+    #Define location to add all the moved fish
+    zero_index <- which(moved$moving == 0)
+    ff_melt[update_these[zero_index], 'value'] <- moved[zero_index, 'moved']
 
     nfish_moved[[zz]] <- moved
   }
 
-  #use this to check stuff
-  #Things that will check
-  # ff_check <- melt(ff)
-  # ff_orig <- melt(ff_orig)
+  #Remove duplicated rows
+  nfish_moved <- ldply(nfish_moved)
 
-  # ff2 <- inner_join(ff_check, ff_orig,  by = c("Var1", "Var2"))
-  # ff2$diff <- ff2$value.y - ff2$value.x
-  return(list(fish_area = ff, nfish_moved = nfish_moved))
+  #Check duplicated
+  duped <- which(duplicated(nfish_moved[, c('x', 'y')]))
+  nfish_moved <- nfish_moved[-duped, ]
+
+  #replace with 
+  ff_melt$unq <- paste(ff_melt$x, ff_melt$y)
+  nfish_moved$unq <- paste(nfish_moved$x, nfish_moved$y)
+
+  nfish_moved$check <- ff_melt[ff_melt$unq %in% nfish_moved$unq, 'value']
+
+  return(list(fish_area = ff_melt, nfish_moved = nfish_moved))
 
 }
 
