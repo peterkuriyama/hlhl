0b1ab59cceb18508adb09528d7514ddf8b50719d
peterkuriyama <ptrkrym@uw.edu>
2017-01-30 11:02:45 -0800

    remove merge conflict at beginning of for loop

--- R/fish_population.R
+++ R/fish_population.R
@@ -58,45 +58,6 @@ fish_population <- function(fish_area, ctl, kk = 0){
   fish_area_list <- lapply(fish_area, FUN = function(x) melt(x))
 
   ##---------------------------------------------------------------------------------------
-  #Fish in Locations
-  ##---------------------------------------------------------------------------------------
-
-# <<<<<<< HEAD
-#   for(ii in 1:nrow(location)){    
-#     row_range <- (location[ii, 'x'] - scope):(location[ii, 'x'] + scope)
-#     row_range <- row_range[row_range %in% 1:nrow(fish_area)] #If there's a border case maybe?
-
-#     col_range <- (location[ii, 'y'] - scope):(location[ii, 'y'] + scope)
-#     col_range <- col_range[col_range %in% 1:ncol(fish_area)]
-
-#     #Define range to fish
-#     fish_area_melted <- reshape2::melt(fish_area)
-
-#     #Use melted matrix because fish_area is easier to subset
-#     fish_range_melted <- subset(fish_area_melted, Var1 %in% row_range & Var2 %in% col_range)
-#     fish_location_melted <-subset(fish_area_melted, Var1 == location[ii, 'x'] & Var2 == location[ii, 'y'])
-
-#     #define zero index
-#     zero_index <-  which(fish_range_melted$Var1 == location[ii, 'x'] & fish_range_melted$Var2 == location[ii, 'y'])
-
-#     fish_range <- matrix(fish_range_melted$value, nrow = length(row_range), ncol = length(col_range))
-#     fish_in_loc <- fish_location_melted$value
-
-#     #define number of fish outside
-#     nfish_outside <- sum(fish_range) - fish_in_loc
-
-#     ##---------------------------------------------------------------------------------------
-#     #Move Fish
-#     ##---------------------------------------------------------------------------------------
-#     #Move fish to specified location
-
-#     #Calculate movement probabilities
-#     #currently nothing goes out of specified location
-#     #Movement probabilites depend on number of fish outside relative to
-#     #total fish outside
-#     probs <- fish_range / nfish_outside
-# =======
-  ##---------------------------------------------------------------------------------------
   #Move Fish into locations
   #Call this fish_temp because to keep the original, and
   fish_temp <- lapply(fish_area, FUN = function(z){
