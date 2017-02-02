9cadbe9212f2867ef2a4b5eb85b8160704296dc4
peterkuriyama <ptrkrym@uw.edu>
2017-02-02 10:02:06 -0800

    fix duplicated row issue and check numbers of moving fish

--- R/move_fish_loop.R
+++ R/move_fish_loop.R
@@ -14,6 +14,8 @@ move_fish_loop <- function(location, ff){
   #Store the original data frame to compare
   ff_orig <- ff
 
+# if(sum(is.na(ff_orig)) != 0) browser()
+
   #Store the number of fish that moved
   nfish_moved <- vector('list', length = 3)
 
@@ -21,6 +23,8 @@ move_fish_loop <- function(location, ff){
   ff_melt <- melt(ff)
   names(ff_melt)[1:2] <- c('x', 'y')
 
+
+  #Now calculate the number of fish that move in each location
   for(zz in 1:nrow(location)){
     moves <- define_movement(fish_area = ff, x = location[zz, 'x'],
       y = location[zz, 'y'], scope = scope)
@@ -36,7 +40,7 @@ move_fish_loop <- function(location, ff){
 
     #Subtract moving fish
     ff_melt[update_these, 'value'] <- ff_melt[update_these, 'value'] - moved$moving
-    
+
     #Define location to add all the moved fish
     zero_index <- which(moved$moving == 0)
     ff_melt[update_these[zero_index], 'value'] <- moved[zero_index, 'moved']
@@ -44,21 +48,44 @@ move_fish_loop <- function(location, ff){
     nfish_moved[[zz]] <- moved
   }
 
-  #Remove duplicated rows
-  nfish_moved <- ldply(nfish_moved)
 
-  #Check duplicated
-  duped <- which(duplicated(nfish_moved[, c('x', 'y')]))
+  #Calculate the number of fish moving in each fishing location
+  names(nfish_moved) <- paste0("loc", 1:length(nfish_moved))
+  nfish_moved <- ldply(nfish_moved)
+  names(nfish_moved)[1] <- 'loc'
   
-  if(length(duped) != 0){
-    nfish_moved <- nfish_moved[-duped, ]  
-  }
+  #Here's the number of fish moving into each location
+  moved_in <- nfish_moved %>% group_by(loc) %>% summarize(nmoving = sum(moving)) %>% as.data.frame
+  
+  #Now look at the number of moving fish in each unique location,
+  #Need to do this because there are duplicates
+  no_dupes <- nfish_moved %>% group_by(x, y) %>% summarize(nmoving = sum(moving)) %>% 
+                as.data.frame 
+  
+  #Add in unq columns for joining
+  no_dupes$unq <- paste(no_dupes$x, no_dupes$y)
+  nfish_moved$unq <- paste(nfish_moved$x, nfish_moved$y)
+  
+  #Join based on each unique location
+  nfish_moved1 <- left_join(nfish_moved[-which(duplicated(nfish_moved$unq)), ], 
+            no_dupes[, c('nmoving', 'unq')], by = 'unq')
+  nfish_moved1$moving <- nfish_moved1$nmoving
+  nfish_moved1$moved <- nfish_moved1$value - nfish_moved1$moving
+  nfish_moved1$nmoving <- NULL
+  
+  #Update moved column with moved_in
+  fish_locs <- which(nfish_moved1$moving == 0)
+  nfish_moved1[fish_locs, 'moved'] <- nfish_moved1[fish_locs, 'value'] + moved_in$nmoving
+  
+  #Clean up the data frames
+  nfish_moved1$loc <- NULL
+  nfish_moved1$unq <- NULL
 
   #replace with 
   ff_melt$unq <- paste(ff_melt$x, ff_melt$y)
-  nfish_moved$unq <- paste(nfish_moved$x, nfish_moved$y)
+  nfish_moved1$unq <- paste(nfish_moved1$x, nfish_moved1$y)
 
-  nfish_moved$check <- ff_melt[ff_melt$unq %in% nfish_moved$unq, 'value']
+  nfish_moved1$check <- ff_melt[ff_melt$unq %in% nfish_moved1$unq, 'value']
 
   return(list(fish_area = ff_melt, nfish_moved = nfish_moved))
 
