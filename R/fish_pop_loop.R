6c78770f97ab2086ab1c6ea1b0387d805e7bf765
peterkuriyama <ptrkrym@uw.edu>
2017-02-02 10:37:18 -0800

    remove debugging scraps

--- R/fish_pop_loop.R
+++ R/fish_pop_loop.R
@@ -15,9 +15,9 @@ fish_pop_loop <- function(fish_area, loc_row, ctl = ctl, kk = 0){
 
     fish1 <- fish_area[[1]][temp_loc[1], temp_loc[2]]
     fish2 <- fish_area[[2]][temp_loc[1], temp_loc[2]]
-# if(kk == 5) cat('fish1=', fish1, 'fish2=', fish2, '\n')
-fish1orig <- fish1    
-fish2orig <- fish2
+
+    fish1orig <- fish1    
+    fish2orig <- fish2
 
     tot_fish <- fish1 + fish2
     nsamps <- ctl$nhooks * ctl$nangs
@@ -25,12 +25,6 @@ fish2orig <- fish2
     #Store numbers of fish caught
     temp_fish12 <- data.frame(nsamps = 1:nsamps, fish1 = rep(999, nsamps),
       fish2 = rep(999, nsamps))
-
-#Do something to make sure the number of samples can't exceed the number of fish there
-#I think looping through this thing for each individual hook leads to it being too 
-    #easy to catch fish
-    #Need to rewrite this as a binomial of nhooks
-# if(ctl$browser) browser()
       
       for(nn in 1:nsamps){                    
         temp_samp <- sample_exp(nfish1 = fish1, nfish2 = fish2, 
@@ -74,40 +68,3 @@ fish2orig <- fish2
 
 }
 
-
-# if(sum(temp_fish12$fish1) > fish_area[[1]][temp_loc[1], temp_loc[2]] | sum(temp_fish12$fish2) > fish_area[[2]][temp_loc[1], temp_loc[2]]) browser()    
-    # fish_area[[1]][temp_loc[1], temp_loc[2]] <- fish_area[[1]][temp_loc[1], temp_loc[2]] - 
-    #   sum(temp_fish12$fish1)
-    # fish_area[[2]][temp_loc[1], temp_loc[2]] <- fish_area[[2]][temp_loc[1], temp_loc[2]] - 
-    #   sum(temp_fish12$fish2)
-
-
-
-
-#Keep for debugging
-  # for(pp in 1:nrow(location)){
-  #   temp_loc <- location[pp, c('x', 'y')]
-  #   fish1 <- fish_area[[1]][temp_loc$x, temp_loc$y]
-  #   fish2 <- fish_area[[2]][temp_loc$x, temp_loc$y]
-
-  #   tot_fish <- fish1 + fish2
-  #   if(tot_fish == 0) next
-
-  #   #Do this for 15 hooks per drop
-  #   nsamps <- nhooks * ctl$nangs
-
-  #   #Store number of fish 1 and 2 caught
-  #   temp_fish12 <- data.frame(nsamps = 1:nsamps, fish1 = rep(999, nsamps), 
-  #     fish2 = rep(999, nsamps))
-
-  #   for(nn in 1:nsamps){
-  #     temp_samp <- sample_exp(nfish1 = fish1, nfish2 = fish2, 
-  #       prob1 = ctl$prob1, prob2 = ctl$prob2)
-  #     temp_fish12[nn, 2:3] <- temp_samp
-
-  #     #update counts of fish1 and fish2
-  #     fish1 <- fish1 - temp_samp$fish1
-  #     fish2 <- fish2 - temp_samp$fish2
-  #   }  
-
-  # }
