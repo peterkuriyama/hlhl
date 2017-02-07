989838f71c8ed66c603a5bbc692dec0b768242ba
peterkuriyama <ptrkrym@uw.edu>
2017-02-07 11:58:59 -0800

    update function

--- R/fish_pop_loop.R
+++ R/fish_pop_loop.R
@@ -11,6 +11,7 @@
 #Input fish_area, one row of location data frame and the ctl file
 
 fish_pop_loop <- function(fish_area, loc_row, ctl = ctl, kk = 0){
+
     temp_loc <- c(loc_row$x, loc_row$y)
 
     fish1 <- fish_area[[1]][temp_loc[1], temp_loc[2]]
