eac9fa43b54935a20fc64fa22433619f91400839
peterkuriyama <ptrkrym@uw.edu>
2017-02-08 12:32:17 -0800

    Update no movement functions

--- R/move_fish_none.R
+++ R/move_fish_none.R
@@ -3,12 +3,14 @@
 #' This function doesn't move any fish. Easiest to add no movement into the package
 #' with this dummy function
 #' @param fish_area Input matrix of distributed fish
+#' @param max_prob not applicable to this function
+#' @param min_prob not applicable to this function
 #' @keywords movement
 #' @export
 #' @examples 
 #' 
 
-move_fish_none <- function(fish_area, ...){
+move_fish_none <- function(fish_area, max_prob, min_prob){
   final <- fish_area
   return(list(init = fish_area, final = final))
 }
