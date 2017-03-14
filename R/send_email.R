e144c98e61ffc75aed41176ebc00da7a7467fa8e
peterkuriyama <ptrkrym@uw.edu>
2017-03-14 13:27:18 -0700

    add function to send email

---
+++ R/send_email.R
@@ -0,0 +1,13 @@
+#' Send email that run is done
+#' Function to send an email, arguments won't match
+#' @param ctl Control file
+#' @param nbest Number of best/good sites to samle
+#' @param nmed Number of medium sites to samle
+#' @param nbest Number of bad sites to samle
+#' @export
+
+send_email <- function(from = "<pkuriyama@gmail.com>", to = "<pkuriyama@gmail.com>",
+  subject = "run done", body = 'run done', mailcontrol = list(smtpServer="ASPMX.L.GOOGLE.COM")){
+ 
+  sendmail(from=from,to=to,subject=subject,msg=body,control=mailControl)
+}
