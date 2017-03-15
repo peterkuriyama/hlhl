60e638190120cdde2335a620fe28f34a4e9b1df9
peterkuriyama <ptrkrym@uw.edu>
2017-03-15 16:22:48 -0700

    clean up code

--- R/conduct_survey.R
+++ R/conduct_survey.R
@@ -13,26 +13,13 @@
 #' conduct_survey(ctl)
 
 conduct_survey <- function(init_area, ...){  
-#Specify movement probabilities
-# max_prob <- ctl$max_prob
-# min_prob <- ctl$min_prob
-  # dots <- list(...)
-  # nyear <- ctl$nyear
-  # survey_samples <- vector('list', length = nyear)
 
   drop_samples <- vector('list', length = nyear)
   fished_areas <- vector('list', length = nyear)
   
-  # names(survey_samples) <- 1:nyear
   names(drop_samples) <- 1:nyear
-  names(fished_areas) <- 1:nyear
-
-  #initialize population  
-  # init_area1 <- initialize_population(ctl = ctl, nfish = ctl$nfish1)
-  # init_area2 <- initialize_population(ctl = ctl, nfish = ctl$nfish2)
-  
+  names(fished_areas) <- 1:nyear  
   #Fish Area Once
-  # init_area <- list(init_area1, init_area2)
   after_first <- fish_population(init_area, ...)
 
   drop_samples[[1]] <- after_first$angler_samples
@@ -68,11 +55,6 @@ conduct_survey <- function(init_area, ...){
   effort <- ctl$nhooks * ctl$nangs * ctl$ndrops
   samples$cpue1 <- samples$fish1samp / effort
   samples$cpue2 <- samples$fish2samp / effort
-
-  #Find columns that don't have 'drop' in the name
-  # to_bind <- samples[, 1:ncol(samples) %in% grep('drop',  names(samples)) == FALSE]
-  
-  # cpue <- cbind(to_bind, cpue)
 
   fished_areas[[nyear + 1]] <- init_area
   names(fished_areas) <- c(paste0('year', 1:nyear), 'year0')
