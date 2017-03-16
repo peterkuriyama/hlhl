3954788433d89d603a3f79b84bcdb5bdb14dfb5a
peterkuriyama <ptrkrym@uw.edu>
2017-03-16 13:55:09 -0700

    unpack dots

--- R/conduct_survey.R
+++ R/conduct_survey.R
@@ -12,16 +12,20 @@
 #' ctl <- make_ctl()
 #' conduct_survey(ctl)
 
-conduct_survey <- function(init_area, ...){  
+conduct_survey <- function(init_area, ...){    
+  nnyear <- list(...)$nyear
+  nnangs <- list(...)$nangs
+  nnhooks <- list(...)$nhook
+  nndrops <- list(...)$ndrops
 
-  drop_samples <- vector('list', length = nyear)
-  fished_areas <- vector('list', length = nyear)
+  drop_samples <- vector('list', length = nnyear)
+  fished_areas <- vector('list', length = nnyear)
   
-  names(drop_samples) <- 1:nyear
-  names(fished_areas) <- 1:nyear  
-  
+  names(drop_samples) <- 1:nnyear
+  names(fished_areas) <- 1:nnyear  
+
   #Fish Area Once
-  after_first <- fish_population(fish_area = init_area, ...)
+  after_first <- fish_population(fish_area = init_area,...)
 
   drop_samples[[1]] <- after_first$angler_samples
   fished_areas[[1]] <- after_first$updated_area
@@ -32,13 +36,11 @@ conduct_survey <- function(init_area, ...){
   # movement_function <- ctl$movement_function
   
   #Loop over years of survey, specified in ctl
-  for(kk in 2:nyear){
+  for(kk in 2:nnyear){
   
     #Leave this in case I want to add movement in 
-    #Move fish based on function specified in ctl 
     # temp_area[[1]] <- movement_function(temp_area[[1]], max_prob = ctl$max_prob, min_prob = ctl$min_prob)$final
     # temp_area[[2]] <- movement_function(temp_area[[2]], max_prob = ctl$max_prob, min_prob = ctl$min_prob)$final    
-# browser()
     temp <- fish_population(fish_area = temp_area, ...)
 
     # survey_samples[[kk]] <- temp$samples
@@ -55,12 +57,12 @@ conduct_survey <- function(init_area, ...){
   names(samples)[1] <- 'year'
 
   #calculate CPUE based on number of hooks
-  effort <- nhooks * nangs * ndrops
+  effort <- nnhooks * nnangs * nndrops
   samples$cpue1 <- samples$fish1samp / effort
   samples$cpue2 <- samples$fish2samp / effort
 
-  fished_areas[[nyear + 1]] <- init_area
-  names(fished_areas) <- c(paste0('year', 1:nyear), 'year0')
+  fished_areas[[nnyear + 1]] <- init_area
+  names(fished_areas) <- c(paste0('year', 1:nnyear), 'year0')
   
   #define output list
   out <- list(fished_areas = fished_areas, samples = samples)
