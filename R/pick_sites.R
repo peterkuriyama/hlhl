1b3ec2a6f63d510ee6fe16b394f2d896cec8a724
peterkuriyama <ptrkrym@uw.edu>
2017-03-20 14:51:44 -0700

    add condition if intervals 1:3 have 0 fish

--- R/pick_sites.R
+++ R/pick_sites.R
@@ -20,11 +20,22 @@ pick_sites <- function(nbest = 0, nmed = 0, nbad = 0, fish_mat){
     fishes[which(fishes$value == quantile(fishes$value)[2]), 'int'] <- 1
   }
   
+  #What to do if there is no middle quantile?
+  
+  #if three of these things
+  if(sum(which(quantile(fishes$value) == 0) %in% 1:3) == 3){
+    print('med and bad are the same')
+    best <- sample(which(fishes$int >= 4), size = nbest, replace = FALSE)
+    med <- sample(which(fishes$int == 3), size = nmed, replace = FALSE)
+    bad <- sample(which(fishes$int == 1 | fishes$int == 2), size = nbad, replace = FALSE)
+  }
 
+  if(sum(which(quantile(fishes$value) == 0) %in% 1:3) != 3){
   #Sample sites based on quantiles
-  best <- sample(which(fishes$int >= 3), size = nbest, replace = FALSE)
-  med <- sample(which(fishes$int == 2), size = nmed, replace = FALSE)
-  bad <- sample(which(fishes$int == 1), size = nbad, replace = FALSE)
+    best <- sample(which(fishes$int >= 3), size = nbest, replace = FALSE)
+    med <- sample(which(fishes$int == 2), size = nmed, replace = FALSE)
+    bad <- sample(which(fishes$int == 1), size = nbad, replace = FALSE)
+  }
 
   locs_out <- fishes[c(best, med, bad), ]
   locs_out$vessel <- 1
