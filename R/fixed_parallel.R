d31703134cfae90768691ee18b73a9486859e0a9
peterkuriyama <ptrkrym@uw.edu>
2017-04-04 11:11:59 -0700

    add function fixed_parallel

---
+++ R/fixed_parallel.R
@@ -0,0 +1,39 @@
+#' Function that takes fixed inputs and runs scenarios
+
+#' Function that calls run_scenario. Allows for parallelization across greater number of cores
+#' @param index Index of to_loop to subset
+#' @param ctl1 Starting ctl list
+#' @export
+
+fixed_parallel <- function(index, ctl1){
+  ctl_temp <- ctl1
+  tt <- to_loop[index, ]
+  ctl_temp$nfish1 <- tt$nfish1
+  ctl_temp$nfish2 <- tt$nfish2
+  ctl_temp$shapes <- c(shape_list1[tt$shape_list_row, 2], shape_list1[tt$shape_list_row, 3])
+  ctl_temp$comp_coeff <- tt$comp_coeff
+
+  #Pick sites based on initial probabilites
+  locs <- vector('list', length = tt$nreps)
+  init1 <- initialize_population_prob(ctl = ctl_temp)
+  for(jj in 1:tt$nreps){
+    locs[[jj]] <- pick_sites_prob(nsites = tt$nsites, samp_option = tt$type,
+      fish_mat = init1)
+  }
+
+  #Run the simulation with the locations 
+  #Only keep the stuff for plot
+  outs <- run_scenario(ctl_start = ctl_temp, loop_over = locs, add_index = TRUE,
+                       par_func = 'change_two', to_change = 'location')[[3]]
+
+  #Format the results to have the right information for plots
+  outs$comp_coeff <- tt$comp_coeff
+  outs$init_dist <- shape_list1[tt$shape_list_row, 'scen']
+  outs$for_plot <- shape_list1[tt$shape_list_row, 'for_plot']
+  outs$type <- tt$type
+  outs$nsites <- tt$nsites
+  outs$iter <- gsub('loc_case', "", outs$location)
+  outs <- outs %>% filter(year == 1) #Filter results to only keep year 1 results
+  outs$location <- NULL
+  return(outs)
+}
