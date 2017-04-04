fd85107416eec5b870a2da1c7640d73672c0d423
peterkuriyama <ptrkrym@uw.edu>
2017-04-04 09:31:37 -0700

    add function to initialize population probabilities, used to select sites for two species imulations

---
+++ R/initialize_population_prob.R
@@ -0,0 +1,42 @@
+#' Initialize Population Probabilities
+#'
+#' Initialize the spatial distribution of the fish population, units are probabilities. Used to select
+#' sites for simulations with two species.
+
+#' @param ctl List of control parameters from make_ctl function, description of arguments in 
+#' make_ctl function
+#' @param nfish Number of fish, use this to generate matrices for both species
+
+#' @export
+
+initialize_population_prob <- function(ctl, nfish){
+  nfish_orig <- nfish
+  numrow <- ctl$numrow
+  numcol <- ctl$numcol
+  # nfish <- ctl$nfish
+  distribute <- ctl$distribute
+  maxfish <- ctl$maxfish
+  percent <- ctl$percent
+  # seed <- ctl$seed
+  area <- ctl$area
+
+  #---------------------------------------------------------------------------------------------------------
+  #Beta distributed fish distribution
+  if(distribute == 'beta'){
+    #reset seed for beta function
+    set.seed(ctl$seed)
+    
+    #Fill in matrix of fish
+    bsamps <- rbeta(ctl$numrow * ctl$numcol, shape1 = ctl$shapes[1], shape2 = ctl$shapes[2])
+    bsamps <- bsamps / sum(bsamps)
+    
+    #Export file
+    fishArea <- matrix(bsamps, nrow = ctl$numrow, ncol = ctl$numcol, byrow = FALSE)
+
+  }
+
+  return(fishArea)
+}
+
+
+
