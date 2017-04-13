bb3825a2573036a403ef797806d21ec4ee4e1254
peterkuriyama <ptrkrym@uw.edu>
2017-04-13 12:05:31 -0700

    add change_these vector to change whatever

--- R/fixed_parallel.R
+++ R/fixed_parallel.R
@@ -5,7 +5,8 @@
 #' @param ctl1 Starting ctl list
 #' @export
 
-fixed_parallel <- function(index, ctl1, to_loop = to_loop){
+fixed_parallel <- function(index, ctl1, to_loop = to_loop, change_these){
+
   #Define shape_list internally to the function
   shape_list1 <- data.frame(scen = c('leftskew', 'rightskew', 'normdist', 'uniform', 'patchy'),
                             shapes1 = c(10, 1, 5, 1, .1),
@@ -14,11 +15,15 @@ fixed_parallel <- function(index, ctl1, to_loop = to_loop){
     
   ctl_temp <- ctl1
   tt <- to_loop[index, ]
-  ctl_temp$nfish1 <- tt$nfish1
-  ctl_temp$nfish2 <- tt$nfish2
-  ctl_temp$shapes <- c(shape_list1[tt$shape_list_row, 2], shape_list1[tt$shape_list_row, 3])
-  ctl_temp$comp_coeff <- tt$comp_coeff
 
+  #Modify ctl_temp based on names of to_loop
+  for(cc in 1:length(change_these)){
+    change_these[cc]
+    ctl_temp[change_these[cc]] <- tt[1, change_these[cc]]
+  }
+  
+  ctl_temp$shapes <- c(shape_list1[tt$shape_list_row, 2], shape_list1[tt$shape_list_row, 3])
+  
   #Pick sites based on initial probabilites
   locs <- vector('list', length = tt$nreps)
   init1 <- initialize_population_prob(ctl = ctl_temp)
@@ -33,16 +38,20 @@ fixed_parallel <- function(index, ctl1, to_loop = to_loop){
                        par_func = 'change_two', to_change = 'location')[[3]]
 
   #Format the results to have the right information for plots
-  outs$comp_coeff <- tt$comp_coeff
   outs$init_dist <- shape_list1[tt$shape_list_row, 'scen']
   outs$for_plot <- shape_list1[tt$shape_list_row, 'for_plot']
   outs$type <- tt$type
   outs$nsites <- tt$nsites
   outs$iter <- gsub('loc_case', "", outs$location)
+  outs$index <- index
+
+  #Add on change_these in a for loop
+  for(cc in 1:length(change_these)){
+    outs[change_these[cc]] <- tt[1, change_these[cc]]  
+  }
+
   outs <- outs %>% filter(year == 1) #Filter results to only keep year 1 results
   outs$location <- NULL
-  outs$index <- index
-  outs$nfish1 <- tt$nfish1
-  outs$nfish2 <- tt$nfish2
+  
   return(outs)
 }
