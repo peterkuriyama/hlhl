ea48af783760e0c439f7b80af54e3b4a0c8516ef
peterkuriyama <ptrkrym@uw.edu>
2017-04-23 11:36:53 -0700

    use logistic and exponential functions for competition

--- R/sample_exp.R
+++ R/sample_exp.R
@@ -40,15 +40,28 @@ sample_exp <- function(nfish1, nfish2, prob1, prob2, comp_coeff){
     p1a <- p1 / (p1 + p2)  
     fish1 <- rbinom(n = 1, size = 1, prob = p1a)
   }
+
+  propfish1 <- nfish1 / (nfish1 + nfish2)
   
-  if(fish == 1 & is.na(comp_coeff) == FALSE & nfish2 != 0){
-    #Use the proportion of fish 1 to adjust the competition coefficient
-    #Default value of 5 for rate of increase
-    propfish1 <- nfish1 / (nfish1 + nfish2)
-    adj_comp_coeff <- comp_coeff * (1 - exp(-5 * propfish1))    
+  #Sampled directly in proportion to numbers
+  if(fish == 1 & comp_coeff == 0.5){
+    fish1 <- rbinom(n = 1, size = 1, prob = propfish1)
+    if(propfish1 == 1) fish1 <- 1
+  }
+
+  #Hyperstable, favor species 1
+  if(fish == 1 & comp_coeff == 0.7){
+    adj_comp_coeff <- 1 - exp(-6 * propfish1)
     fish1 <- rbinom(n = 1, size = 1, prob = adj_comp_coeff)
   }
-  
+
+  #Hyperdepletion, favor species 2
+  if(fish == 1 & comp_coeff == 0.3){
+    adj_comp_coeff <- log(1 - propfish1) / -6 #inverse of function 3
+    if(propfish1 == 1) fish1 <- 1
+    fish1 <- rbinom(n = 1, size = 1, prob = adj_comp_coeff)
+  }
+
   if(fish1 == 0 & fish == 1 & nfish2 != 0){    
     fish2 <- 1
   } 
@@ -57,3 +70,45 @@ sample_exp <- function(nfish1, nfish2, prob1, prob2, comp_coeff){
   return(data.frame(fish1 = fish1, fish2 = fish2))
   
 }
+
+#Test the function
+# sample_exp(nfish1 = 100, nfish2 = 200, prob1 = .01, prob2 = .01, comp_coeff = 0.3)
+
+# nsamps <- 100
+# fish1 <- 300
+# fish2 <- 100
+# prob1 <- .01
+# prob2 <- .01
+# comp_coeff <- 0.3
+
+# temp_fish12 <- data.frame(nsamps = 1:nsamps, fish1 = rep(999, nsamps),
+#       fish2 = rep(999, nsamps))
+      
+# for(nn in 1:nsamps){                    
+#   temp_samp <- sample_exp(nfish1 = fish1, nfish2 = fish2, 
+#     prob1 = prob1, prob2 = prob2, comp_coeff = comp_coeff)
+
+#   #Make sure that catch of fish can't exceed number of fish      
+#   if(fish1 - temp_samp$fish1 < 0) {
+#     print('sp1')
+#     temp_samp$fish1 <- 0
+#   }
+
+#   if(fish2 - temp_samp$fish2 < 0){
+#     print('sp2')
+#     # browser()
+#     temp_samp$fish2 <- 0
+#   }
+
+#   temp_fish12[nn, 2:3] <- temp_samp
+  
+#   #update counts of fish1 and fish2
+#   fish1 <- fish1 - temp_samp$fish1
+#   fish2 <- fish2 - temp_samp$fish2
+
+#   #Change the probabilities if there aren't any more fish
+#   if(fish1 == 0) prob1 <- 0
+#   if(fish2 == 0) prob2 <- 0
+# }
+
+# colSums(temp_fish12)
