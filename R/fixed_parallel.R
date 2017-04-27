f48096b76acd9aac529e613e6064d97f9ed46f79
peterkuriyama <ptrkrym@uw.edu>
2017-04-27 16:49:14 -0700

    update site selection and prob calculations

--- R/fixed_parallel.R
+++ R/fixed_parallel.R
@@ -6,7 +6,7 @@
 #' @export
 
 fixed_parallel <- function(index, ctl1, to_loop = to_loop, change_these){
-
+  #--------------------------------------------------------------------
   #Define shape_list internally to the function
   shape_list1 <- data.frame(scen = c('leftskew', 'rightskew', 'normdist', 'uniform', 'patchy'),
                             shapes1 = c(10, 1, 5, 1, .1),
@@ -15,22 +15,41 @@ fixed_parallel <- function(index, ctl1, to_loop = to_loop, change_these){
   ctl_temp <- ctl1
   tt <- to_loop[index, ]
 
+  #--------------------------------------------------------------------
   #Modify ctl_temp based on names of to_loop
   for(cc in 1:length(change_these)){
     change_these[cc]
     ctl_temp[change_these[cc]] <- tt[1, change_these[cc]]
   }
-  
+
+  #Change prob1 and prob2
+  ctl_temp$prob1 <- tt$c1_sum * tt$comp_coeff
+  ctl_temp$prob2 <- tt$c1_sum - ctl_temp$prob1
+
   ctl_temp$shapes <- c(shape_list1[tt$shape_list_row, 2], shape_list1[tt$shape_list_row, 3])
-  
+
+  #--------------------------------------------------------------------
   #Pick sites based on initial probabilites
   locs <- vector('list', length = tt$nreps)
-  init1 <- initialize_population_prob(ctl = ctl_temp)
+
+  #Pick sites based on total number of fish
+  # init1 <- initialize_population_prob(ctl = ctl_temp)
+  c1 <- ctl_temp$prob1 / (ctl_temp$prob1 + ctl_temp$prob2)
+
+  #add the two spp together
+  initspp1 <- initialize_population(ctl = ctl_temp, nfish = ctl_temp$nfish1)
+  initspp2 <- initialize_population(ctl = ctl_temp, nfish = ctl_temp$nfish2)
+
+  #Pick sites based on the combined number of fish species
+  init_prop <- initspp1 + initspp2
+  init_prop <- init_prop / sum(init_prop)
+  
   for(jj in 1:tt$nreps){
     locs[[jj]] <- pick_sites_prob(nsites = tt$nsites, samp_option = tt$type,
-      fish_mat = init1)
+      fish_mat = init_prop)
   }
 
+  #--------------------------------------------------------------------
   #Run the simulation with the locations 
   #Only keep the stuff for plot
   outs <- run_scenario(ctl_start = ctl_temp, loop_over = locs, add_index = TRUE,
