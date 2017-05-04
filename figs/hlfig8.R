2f896fb0423bea1fb68db7b6f1b4295c80e99e32
peterkuriyama <ptrkrym@uw.edu>
2017-05-04 15:53:12 -0700

    add line for area with impossible cpue

--- figs/hlfig8.R
+++ figs/hlfig8.R
@@ -28,37 +28,58 @@ ggplot(plot8, aes(x = cpue_boca, y = cpue_verm)) +
   facet_wrap(~ Year) + theme_bw()
 
 png(width = 8, height = 7, units = 'in', res = 150, file = 'figs/hlfig8.png')
+
 par(mfrow = c(3, 4),mar = c(0, 0, 0, 0), oma = c(3.5, 5, 2, 1), mgp = c(1, .5, 0),
   xpd = T)
 for(ii in 1:length(yrz)){
   tp8 <- plot8 %>% filter(Year == yrz[[ii]])
 
   plot(1:10, xlim = c(0, .85), ylim = c(0, .95), type = 'n', ann = F, axes = F)
-  # plot(1:10, xlim = c(-.05, .8), ylim = c(0, .8), type = 'n', ann = F, axes = F, xaxs = 'i', 
-  #   yaxs = 'i')
+  # gray_color <- rgb(t(col2rgb('black')), maxColorValue = 255, alpha = 75)
+  gray_color <- 'gray80'
+  #Add polygon in impossible areas
+
+  lines(x = seq(0, .9, .1), y = 1 - seq(0, .9, .1), lty = 2, col = 'gray')
+  # polygon(x = c(0, .9, .9, 0), y = c(1, 1, .1, 1), col = gray_color, 
+  #   border = gray_color)
   box()
-  
-  points(tp8$cpue_boca, tp8$cpue_verm, pch = 21, cex = tp8$cpue_tot * 3, 
+
+  #Point size corresponds to   
+  # points(tp8$cpue_boca, tp8$cpue_verm, pch = 21, cex = tp8$cpue_tot * 3, 
+  #   bg = rgb(t(col2rgb('black')), maxColorValue = 255, alpha = 75), 
+  #   col = NA)
+
+  points(tp8$cpue_boca, tp8$cpue_verm, pch = 21,
     bg = rgb(t(col2rgb('black')), maxColorValue = 255, alpha = 75), 
-    col = NA)
+    col = NA, cex = 1)
 
   mtext(letts7[ii], side = 3, line = -1.5, adj = .02, cex = .9)
   
-  if(ii == 1) legend('topright', legend = rev(c(.25, .50, .75, 1)), 
-    pt.cex = rev(c(.75, 1.5, 2.25, 3)), pch = 19, 
-    col = rgb(t(col2rgb('black')), maxColorValue = 255, alpha = 75), 
-    bty = 'n', y.intersp = 1.2, cex = 1)
+  # if(ii == 1) legend('topright', legend = rev(c(.25, .50, .75, 1)), 
+  #   pt.cex = rev(c(.75, 1.5, 2.25, 3)), pch = 19, 
+  #   col = rgb(t(col2rgb('black')), maxColorValue = 255, alpha = 75), 
+  #   bty = 'n', y.intersp = 1.2, cex = 1)
 
-  if(ii %in% c(1, 5, 9)) axis(side = 2, at = c(0, .2, .4, .6, .8), las = 2, 
-    cex.axis = 1.2)
-  if(ii >= 8) axis(side = 1, at = c(0, .2, .4, .6, .8), cex.axis = 1.2)
   #Add Axes
+  if(ii %in% c(1, 5, 9)) axis(side = 2, at = c(0, .2, .4, .6, .8), las = 2, 
+    cex.axis = 1.2, labels = c("0", "0.2", "0.4", "0.6", "0.8"))
+  if(ii >= 8) axis(side = 1, at = c(0, .2, .4, .6, .8), cex.axis = 1.2, 
+    labels = c("0", "0.2", "0.4", "0.6", "0.8"))
 }
 
 mtext("Bocaccio CPUE", side = 1, outer = T, cex = 1.2, line = 2.2)
 mtext("Vermilion CPUE", side = 2, outer = T, cex = 1.2, line = 2.2)
 
+
 dev.off()
+
+
+
+
+
+
+
+
 
 
 
