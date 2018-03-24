db3b0b98d6ea58d8ca1ec1df124d198bcb248fc2
peterkuriyama <ptrkrym@uw.edu>
2018-03-23 19:02:21 -0700

    add ability to increase the number of fish in locations

--- R/conduct_survey.R
+++ R/conduct_survey.R
@@ -17,13 +17,95 @@ conduct_survey <- function(init_area, ...){
   nnangs <- list(...)$nangs
   nnhooks <- list(...)$nhook
   nndrops <- list(...)$ndrops
-
+  dep_type <- list(...)$dep_type
   drop_samples <- vector('list', length = nnyear)
   fished_areas <- vector('list', length = nnyear)
   
   names(drop_samples) <- 1:nnyear
   names(fished_areas) <- 1:nnyear  
 
+  #--------------------------------------------------------
+  #MOve fish in or out depending on the dep_type specification
+  
+  #Extract fishing locations
+  location <- list(...)$location
+
+  #Extract the initial area to match locations with fishing locations
+  the_init_area <- init_area[[1]]
+  the_init_area <- melt(the_init_area)
+  names(the_init_area) <- c('x', 'y', 'value')
+  the_init_area$unq <- paste(the_init_area$x, the_init_area$y)
+
+  location$unq <- paste(location$x, location$y)
+
+  #Filter the fished locations
+  fished_locs <- which(the_init_area$unq %in% location$unq)
+  #which have fish also
+  locs_with_fish <- which(the_init_area$value != 0)
+
+#Need to specify proportion of fish  
+prop_moving <- .1
+  #----------------------------
+  #Meaning that the fishing locations are in the best habitat  
+  #Fish move into the fishing locations
+  if(dep_type == 'increasing'){
+    #-----Move fish out
+    #Move fish from outside the fishing area into the fishing area
+    fish_out <- the_init_area[-fished_locs, ]
+    
+    #Sample the locations of fish to move
+    nsamps <- round(sum(fish_out$value) * prop_moving)
+
+    fish_samps <- base::sample(x = rep(fish_out$unq, times = fish_out$value), 
+      nsamps, replace = FALSE)
+
+    fish_samps <- melt(table(fish_samps))
+    names(fish_samps) <- c('unq', 'fish_out')
+    fish_out <- fish_out %>% left_join(fish_samps, by = 'unq') %>%
+      replace_na(list(fish_out = 0))
+    fish_out$updated_value <- fish_out$value - fish_out$fish_out
+
+    #-----Move fish in
+    #Add the fish to fishing areas that already have fish 
+    fish_in <- the_init_area[fished_locs, ]
+
+    fish_in_samps <- fish_in %>% filter(value != 0) %>% select(unq) %>%
+      sample_n(size = nsamps, replace = T) %>% group_by(unq) %>% tally() %>%
+      as.data.frame()
+    names(fish_in_samps) <- c('unq', 'fish_in')
+    fish_in <- fish_in %>% left_join(fish_in_samps, by = 'unq') %>%
+      replace_na(list(fish_in = 0))
+    fish_in$updated_value <- fish_in$value + fish_in$fish_in
+
+    #-----Combine the in/out datasets and convert into a matrix for use
+    fish_updated <- rbind(fish_in %>% select(x, y, updated_value, unq),
+                          fish_out %>% select(x, y, updated_value, unq))
+    #sort the data by x and y
+    fish_updated <- fish_updated %>% arrange(y, x)
+    init_area[[1]] <- matrix(fish_updated$updated_value, byrow = F, nrow = 30, ncol = 30)
+
+print(init_area[[1]][1:5, 1:5])
+
+  }
+
+browser()
+  #meaning that the fish in fishing locations are "depleted" or moved out
+  if(dep_type == 'decreasing'){
+
+  }
+
+
+  # matrix(the_init_area$value, byrow = F, nrow = 30, ncol = 30)
+
+
+
+
+
+  #Add or remove fish from sample sites
+  
+
+
+  #--------------------------------------------------------
   #Fish Area Once
   after_first <- fish_population(fish_area = init_area,...)
 
